dd7358648ceadbba124b2341105e8e0bef6128eb
msametyildiz <97543064+msametyildiz@users.noreply.github.com>
2022-09-21 17:14:57 +0300

    update

--- yonetimpaneli/include/duzenle.php
+++ yonetimpaneli/include/duzenle.php
@@ -1,8 +1,10 @@
 <?php
 if (!empty($_GET["tablo"]) && !empty($_GET["ID"])) {
+
   $tablo = $VT->filter($_GET["tablo"]);
   $ID = $VT->filter($_GET["ID"]);
   $kontrol = $VT->VeriGetir("moduller", "WHERE tablo=? AND durum=?", array($tablo, 1), "ORDER BY ID ASC", 1);
+  
   if ($kontrol != false) {
     $veri = $VT->VeriGetir($kontrol[0]["tablo"], "WHERE ID=?", array($ID), "ORDER BY ID ASC", 1);
     if ($veri != false) {
