cca98def2a4945fb4172ba6f8e74774cbb16dbcb
msametyildiz <97543064+msametyildiz@users.noreply.github.com>
2022-10-14 16:13:07 +0300

    update
    added the new parts and same modules

--- yonetimpaneli/class/VT.php
+++ yonetimpaneli/class/VT.php
@@ -307,9 +307,32 @@ class VT{
 			return false;
 		}
 	}
-
-
- 
+//--------------------------------------------------------------------------------------------------------------------------------------
+	public function MailGonder($mail,$konu="",$mesaj)
+	{
+			$posta = new PHPMailer();
+				$posta->CharSet = "UTF-8";
+			 $posta->IsSMTP();                                   // send via SMTP
+			 $posta->Host     = 	"mail.siteadi.com"; // SMTP servers
+			 $posta->SMTPAuth = true;     // turn on SMTP authentication
+			 $posta->Username = "mail@siteadi.com";  // SMTP username
+			 $posta->Password = "mailsifresi"; // SMTP password
+			 $posta->Port     = 587; 
+			 $posta->From     = "mail@siteadi.com"; // smtp kullanýcý adýnýz ile ayný olmalý
+			 $posta->Fromname = "mail@siteadi.com";
+			 $posta->AddAddress($mail, "mail@siteadi.com");
+			 $posta->Subject  =  $konu;
+			 $posta->Body     =  $mesaj;
+			 
+			 if(!$posta->Send())
+			 {
+			   return false;
+			 }
+			 else
+			 {
+				 return true;
+			 }
+	}
 
 
 }
