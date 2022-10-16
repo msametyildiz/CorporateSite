fae70a97e118ff7108609d64e3ce94e46411b63d
msametyildiz <97543064+msametyildiz@users.noreply.github.com>
2022-10-16 21:33:20 +0300

    update
    added the new parts and files

--- data/ust.php
+++ data/ust.php
@@ -1,63 +1,63 @@
 <!-- header-start -->
-    <header>
-        <div class="header-area ">
-            <div id="sticky-header" class="main-header-area">
-                <div class="container-fluid ">
-                    <div class="header_bottom_border">
-                        <div class="row align-items-center">
-                            <div class="col-xl-3 col-lg-2">
-                                <div class="logo">
-                                    <a href="<?=SITE?>" style="color:black;font-size:30px;background-image: url('<?=SITE?>img/about/counter.png')">
-                                        PAYLASTIKCA
-                                        <!--<img src="<?=SITE?>img/logo.png" alt="">-->
-                                    </a>
-                                </div>
-                            </div>
-                            <div class="col-xl-6 col-lg-7">
-                                <div class="main-menu  d-none d-lg-block">
-                                    <nav>
-                                        <ul id="navigation">
-                                            <li><a href="<?=SITE?>">ANASAYFA</a></li>
-                                            <li><a href="#">KURUMSAL <i class="ti-angle-down"></i></a>
-                                                <ul class="submenu">
-                                                    <?php
-                                                        $kurumsal=$VT->VeriGetir("kurumsal","WHERE durum=?",array(1),"ORDER BY sirano ASC");
- //(LıNE 23)  kurumsal tablosundan alınmasını istedim,  "eğer durumu = 1 olan kısımı getir diyorum",sıra numarasında göre  sıralamasını istediğimi belirttim. herhangi bir limit işlemi yapmıyorum
-                                                    if($kurumsal!=false){//eğer bir değer var ise
-                                                        for($i=0;$i<count($kurumsal);$i++){
-                                                            ?>
-                                                            <li><a href="<?=SITE?>kurumsal/<?=$kurumsal[$i]["selflink"]?>"><?=stripslashes($kurumsal[$i]["baslik"])?></a></li>
-                                                            <?php
-                                                        }
-                                                    }
-                                                    ?>
-                                                </ul>
-                                            </li>
-                                            <!--hizmetlerimin yada başka bir sayfanın altına bir şey kelemek için yani açılır menü yapmak için href kısmına # ekle -->
-                                            <li><a href="<?=SITE?>hizmetler">HİZMETLER</a></li>
-                                            <li><a href="<?=SITE?>projeler">PROJELER</a></li>
-                                            <li><a href="<?=SITE?>blog">BLOG</a></li>
-                                            <li><a href="<?=SITE?>iletisim">İLETİŞİM</a></li>
-                                        </ul>
-                                    </nav>
-                                </div>
-                            </div>
-                            <div class="col-md- col-sm- d-none d-lg-block" >
-                                <div class="Appointment">
-                                    <div class="book_btn d-none d-lg-block">
-                                        <a href="<?=SITE?>iletisim">BİZE ULAŞIN</a>
-                                    </div>
-                                </div>
-                            </div>
-                    
-                            <div class="col-12">
-                                <div class="mobile_menu d-block d-lg-none"></div>
+<header>
+    <div class="header-area ">
+        <div id="sticky-header" class="main-header-area">
+            <div class="container-fluid ">
+                <div class="header_bottom_border">
+                    <div class="row align-items-center">
+                        <div class="col-xl-3 col-lg-2">
+                            <div class="logo">
+                                <a href="<?= SITE ?>" style="color:black;font-size:30px;background-image: url('<?= SITE ?>img/about/counter.png')">
+                                    PAYLASTIKCA
+                                    <!--<img src="<?= SITE ?>img/logo.png" alt="">-->
+                                </a>
                             </div>
                         </div>
+                        <div class="col-xl-6 col-lg-7">
+                            <div class="main-menu  d-none d-lg-block">
+                                <nav>
+                                    <ul id="navigation">
+                                        <li><a href="<?= SITE ?>">ANASAYFA</a></li>
+                                        <li><a href="#">KURUMSAL <i class="ti-angle-down"></i></a>
+                                            <ul class="submenu">
+                                                <?php
+                                                $kurumsal = $VT->VeriGetir("kurumsal", "WHERE durum=?", array(1), "ORDER BY sirano ASC");
+                                                //(LıNE 23)  kurumsal tablosundan alınmasını istedim,  "eğer durumu = 1 olan kısımı getir diyorum",sıra numarasında göre  sıralamasını istediğimi belirttim. herhangi bir limit işlemi yapmıyorum
+                                                if ($kurumsal != false) { //eğer bir değer var ise
+                                                    for ($i = 0; $i < count($kurumsal); $i++) {
+                                                ?>
+                                                        <li><a href="<?= SITE ?>kurumsal/<?= $kurumsal[$i]["selflink"] ?>"><?= stripslashes($kurumsal[$i]["baslik"]) ?></a></li>
+                                                <?php
+                                                    }
+                                                }
+                                                ?>
+                                            </ul>
+                                        </li>
+                                        <!--hizmetlerimin yada başka bir sayfanın altına bir şey kelemek için yani açılır menü yapmak için href kısmına # ekle -->
+                                        <li><a href="<?= SITE ?>hizmetler">HİZMETLER</a></li>
+                                        <li><a href="<?= SITE ?>projeler">PROJELER</a></li>
+                                        <li><a href="<?= SITE ?>blog">BLOG</a></li>
+                                        <li><a href="<?= SITE ?>iletisim">İLETİŞİM</a></li>
+                                    </ul>
+                                </nav>
+                            </div>
+                        </div>
+                        <div>
+                            <div style="float: left; padding-left:80px;">
+                                <a href="<?= SITE ?>iletisim" class="buttonIDUst">BİZE ULAŞIN</a>
+                            </div>
+                            <div style="float: right; padding-left:5px;">
+                                <a href="<?= SITE ?>bagis" class="buttonIDUst">BAĞIŞ YAP  <i class="fa fa-heart" aria-hidden="true" style="padding:3px;"></i></a>
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <div class="mobile_menu d-block d-lg-none"></div>
+                        </div>
                     </div>
-
                 </div>
+
             </div>
         </div>
-    </header>
-    <!-- header-end -->
+    </div>
+</header>
+<!-- header-end -->
---
+++ include/bagis.php
@@ -0,0 +1,82 @@
+
+<!-- bradcam_area  -->
+<div class="bradcam_area bradcam_bg_2">
+       <div class="container">
+           <div class="row">
+               <div class="col-xl-12">
+                   <div class="bradcam_text text-center">
+                       <h3>Hizmetlerimiz</h3>
+                   </div>
+               </div>
+           </div>
+       </div>
+   </div>
+   <!--/ bradcam_area  -->
+   
+<!-- about_mission  -->
+<div class="explorer_europe">
+    <div class="container" style="padding-top:7%">
+   <div class="row align-items-center">
+       <div class="section background-white padding-top-small over-hide">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-xl-12">
+                <div class="main-title text-center">
+                    <h3 class="mb-4" style="font-family:Raleway, sans-serif;">Necat Derneği Bağışçısı Ol !</h3>
+                    <div class="main-subtitle-top mb-4">Türkiye İş Bankası</div>
+                    <h6 style="font-weight: bold;">HESAP ADI: Necat Derneği</h6>
+                    <div class="main-subtitle-top mb-4" style="opacity: 0.7;">Necat Derneği’ne yurtdışından bağış yapmak isteyenlerin dikkatine!</div>
+                    <div class="main-subtitle-bottom mt-3" style="opacity: 0.7;">Dünyanın her yerindeki bankalardan Necat derneği resmî banka hesaplarına, döviz havalesi gönderilebilmektedir.</div>
+                    <div class="main-subtitle-bottom mt-3" style="opacity: 0.7;">Yurtdışından yapılacak ödemeler için: Swift Kodu “<strong>ISBKTRIS</strong>”</div>
+                    <div class="mt-3" style="opacity: 0.7;">Necat yardımlarını, projelerini sürdürülebilir kılmak için Necat Derneği’ne ait hesap numaralarımız:</div>
+                </div>
+            </div>
+            <div class="clear"></div>
+            <div class="col-xl-3 col-md-4 col-lg-3" data-scroll-reveal="enter bottom move 40px over 0.8s after 0.2s" data-scroll-reveal-id="1" data-scroll-reveal-initialized="true" data-scroll-reveal-complete="true">
+                <div class="pricing p-xl-5 background-white text-center">
+                    <h5 style="font-family:Raleway, sans-serif;">TL Hesabı</h5>
+                    <div class="pricing-sub mb-4">1021-1380059</div>
+                    <div class="pricing-line mt-4 mb-4"></div>
+                    <h5 style="font-family:Raleway, sans-serif;">IBAN</h5>
+                    <div class="pricing-sub">TR120006400000110211380059</div>
+                </div>
+            </div>
+            <div class="col-xl-3 col-md-4 col-lg-3" data-scroll-reveal="enter bottom move 40px over 0.8s after 0.2s" data-scroll-reveal-id="2" data-scroll-reveal-initialized="true" data-scroll-reveal-complete="true">
+                <div class="pricing p-xl-5 background-white text-center">
+                    <h5 style="font-family:Raleway, sans-serif;">USD Hesabı</h5>
+                    <div class="pricing-sub mb-4">1021-2150262</div>
+                    <div class="pricing-line mt-4 mb-4"></div>
+                    <h5 style="font-family:Raleway, sans-serif;">IBAN</h5>
+                    <div class="pricing-sub">TR320006400000210212150262</div>
+                </div>
+            </div>
+            <div class="col-xl-3 col-md-4 col-lg-3" data-scroll-reveal="enter bottom move 40px over 0.8s after 0.2s" data-scroll-reveal-id="3" data-scroll-reveal-initialized="true" data-scroll-reveal-complete="true">
+                <div class="pricing p-xl-5 background-white text-center">
+                    <h5 style="font-family:Raleway, sans-serif;"> EUR Hesabı</h5>
+                    <div class="pricing-sub mb-4">1021-2150277</div>
+                    <div class="pricing-line mt-4 mb-4"></div>
+                    <h5 style="font-family:Raleway, sans-serif;">IBAN</h5>
+                    <div class="pricing-sub">TR150006400000210212150277</div>
+                </div>
+            </div>
+            <div class="col-xl-3 col-md-4 col-lg-3" data-scroll-reveal="enter bottom move 40px over 0.8s after 0.2s" data-scroll-reveal-id="4" data-scroll-reveal-initialized="true" data-scroll-reveal-complete="true">
+                <div class="pricing p-xl-5 background-white text-center">
+                    <h5 style="font-family:Raleway, sans-serif;">GBP Sterlin Hesabı</h5>
+                    <div class="pricing-sub mb-4">1021-2150277</div>
+                    <div class="pricing-line mt-4 mb-4"></div>
+                    <h5 style="font-family:Raleway, sans-serif;">IBAN</h5>
+                    <div class="pricing-sub">TR370006400000210212260849</div>
+                </div>
+            </div>
+            <div class="col-md-12 text-center" >
+                <a href="/bagis-bildirim-formu" class="buttonIDBagis">Bağış Bildirim Formu</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+        </div>
+    </div>
+</div>
+<!--/ about_mission -->
+ 
