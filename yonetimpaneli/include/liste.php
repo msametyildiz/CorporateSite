451b5366124c7cad3f7bc8bcb6803687c9b6ab35
msametyildiz <97543064+msametyildiz@users.noreply.github.com>
2022-10-16 14:53:09 +0300

    update
    added the new files and parts

--- yonetimpaneli/include/liste.php
+++ yonetimpaneli/include/liste.php
@@ -1,12 +1,12 @@
-<?php 
+<?php
 
-  if(!empty($_GET["tablo"])){
+if (!empty($_GET["tablo"])) {
 
-    $tablo=$VT->filter($_GET["tablo"]);
+  $tablo = $VT->filter($_GET["tablo"]);
 
-    $kontrol=$VT->VeriGetir("moduller","WHERE tablo=? AND durum=?",array($tablo,1),"ORDER BY ID ASC",1);
+  $kontrol = $VT->VeriGetir("moduller", "WHERE tablo=? AND durum=?", array($tablo, 1), "ORDER BY ID ASC", 1);
 
-    if($kontrol!=false){
+  if ($kontrol != false) {
 
 ?>
 
@@ -14,226 +14,225 @@
 
 
 
-<div class="content-wrapper">
+    <div class="content-wrapper">
 
-    <!-- Content Header (Page header) -->
+      <!-- Content Header (Page header) -->
 
-    <div class="content-header">
+      <div class="content-header">
 
-      <div class="container-fluid">
+        <div class="container-fluid">
 
-        <div class="row mb-2">
+          <div class="row mb-2">
 
-          <div class="col-sm-6">
+            <div class="col-sm-6">
 
-            <h1 class="m-0"><?=$kontrol[0]["baslik"]?></h1>
+              <h1 class="m-0"><?= $kontrol[0]["baslik"] ?></h1>
 
-          </div><!-- /.col -->
+            </div><!-- /.col -->
 
-          <div class="col-sm-6">
+            <div class="col-sm-6">
 
-            <ol class="breadcrumb float-sm-right">
+              <ol class="breadcrumb float-sm-right">
 
-              <li class="breadcrumb-item"><a href="<?=SITE?>">Anasayfa</a></li>
+                <li class="breadcrumb-item"><a href="<?= SITE ?>">Anasayfa</a></li>
 
-              <li class="breadcrumb-item active"><?=$kontrol[0]["baslik"]?></li>
+                <li class="breadcrumb-item active"><?= $kontrol[0]["baslik"] ?></li>
 
-            </ol>
+              </ol>
 
-          </div><!-- /.col -->
+            </div><!-- /.col -->
 
-        </div><!-- /.row -->
+          </div><!-- /.row -->
 
-      </div><!-- /.container-fluid -->
-
-    </div>
-
-    <!-- /.content-header -->
-
-
-
-    <!-- Main content -->
-
-    <section class="content">
-
-      <div class="container-fluid">
-
-      <div class="row">
-
-        <div class="col-md-12">
-
-          <a href="<?=SITE?>ekle/<?=$kontrol[0]["tablo"]?>" class="btn btn-success" style="float:right; margin-bottom=10px;"><i class="fa fa-plus"></i>YENİ EKLE</a>
+        </div><!-- /.container-fluid -->
 
       </div>
 
-    </div>
+      <!-- /.content-header -->
 
-      <div class="card">
 
-              <!-- /.card-header -->
 
-              <div class="card-body">
+      <!-- Main content -->
 
-                <table id="example1" class="table table-bordered table-striped">
+      <section class="content">
 
-                  <thead>
+        <div class="container-fluid">
+
+          <div class="row">
+
+            <div class="col-md-12">
+
+              <a href="<?= SITE ?>ekle/<?= $kontrol[0]["tablo"] ?>" class="btn btn-success" style="float:right; margin-bottom=10px;"><i class="fa fa-plus"></i>YENİ EKLE</a>
+
+            </div>
+            <div class="col-md-12"><br></div>
+
+          </div>
+
+          <div class="card">
+
+            <!-- /.card-header -->
+
+            <div class="card-body">
+
+              <table id="example1" class="table table-bordered table-striped">
+
+                <thead>
 
                   <tr>
 
-                      <th style="width:50px;">Sıra</th>
+                    <th style="width:50px;">Sıra</th>
 
-                      <th>Açıklama</th>
+                    <th>Açıklama</th>
 
-                      <th style="width:50px;">Durum</th>
+                    <th style="width:50px;">Durum</th>
 
-                      <th style="width:80px;">Tarih</th>
+                    <th style="width:80px;">Tarih</th>
 
-                      <th style="width:120px;">İşlem</th>
+                    <th style="width:120px;">İşlem</th>
 
                   </tr>
 
-                  </thead>
+                </thead>
 
-                  <tbody>
+                <tbody>
 
-<!-----------------------------------------------------------PHP---------------------------------------------------------------------------> 
+                  <!-----------------------------------------------------------PHP--------------------------------------------------------------------------->
 
-                    <?php
+                  <?php
 
-                      $veriler=$VT->VeriGetir($kontrol[0]["tablo"],"","","ORDER BY ID ASC");
+                  $veriler = $VT->VeriGetir($kontrol[0]["tablo"], "", "", "ORDER BY ID ASC");
 
-                      if($veriler!=false){ 
+                  if ($veriler != false) {
 
-                        $sira=0;
+                    $sira = 0;
 
-                        for($i=0;$i<count($veriler);$i++){
+                    for ($i = 0; $i < count($veriler); $i++) {
 
-                          $sira++;
+                      $sira++;
 
-                          if($veriler[$i]["durum"]==1){$aktifpasif='checked="checked"';}else{$aktifpasif='';}
-
-                          ?>
-
-                              <tr>
-
-                                  <td><?=$sira?></td>
-
-                                    <td><?php
-
-                                       echo stripslashes($veriler[$i]["baslik"]); //stripslashes -->html taglarını temizlemiyor
-
-                                       echo '<br/>'.mb_substr(strip_tags(stripslashes($veriler[$i]["metin"])),0,130,"UTF-8")."...";/*strip_tags -> html taglarını temizliyor*/?></td>
-
-                                  <td>
-
-                                      <div class="custom-control custom-switch custom-switch-off-danger custom-switch-on-success">
-
-                                      <input type="checkbox" class="custom-control-input aktifpasif<?=$veriler[$i]["ID"]?>" id="customSwitch3<?=$veriler[$i]["ID"]?>" <?=$aktifpasif?>  value="<?=$veriler[$i]["ID"]?>" onclick="aktifpasif(<?=$veriler[$i]['ID']?>,'<?=$kontrol[0]['tablo']?>')"; >
-
-                                          <label class="custom-control-label" for="customSwitch3<?=$veriler[$i]["ID"]?>"></label>
-
-                                    </div>
-
-                                  </td>
-
-                                  <td><?=$veriler[$i]["tarih"]?></td>
-
-                                  <td>
-
-                                    <a href="<?=SITE?>duzenle/<?=$kontrol[0]["tablo"]?>/<?=$veriler[$i]["ID"]?>" class="btn btn-warning btn-sm">Düzenle</a>
-
-                                    <a href="<?=SITE?>sil/<?=$kontrol[0]["tablo"]?>/<?=$veriler[$i]["ID"]?>" class="btn btn-danger btn-sm">Kaldır</a>
-
-                                  </td>
-
-                              </tr>
-
-                          <?php
-
-                        }
-
+                      if ($veriler[$i]["durum"] == 1) {
+                        $aktifpasif = 'checked="checked"';
+                      } else {
+                        $aktifpasif = '';
                       }
 
-                    
+                  ?>
 
-                    ?>
+                      <tr>
 
-<!-----------------------------------------------------------/PHP--------------------------------------------------------------------------->
+                        <td><?= $sira ?></td>
 
-                  
+                        <td><?php
 
-                  </tbody>
+                            echo stripslashes($veriler[$i]["baslik"]); //stripslashes -->html taglarını temizlemiyor
 
-                  <tfoot>
+                            echo '<br/>' . mb_substr(strip_tags(stripslashes($veriler[$i]["metin"])), 0, 130, "UTF-8") . "...";/*strip_tags -> html taglarını temizliyor*/ ?></td>
+
+                        <td>
+
+                          <div class="custom-control custom-switch custom-switch-off-danger custom-switch-on-success">
+
+                            <input type="checkbox" class="custom-control-input aktifpasif<?= $veriler[$i]["ID"] ?>" id="customSwitch3<?= $veriler[$i]["ID"] ?>" <?= $aktifpasif ?> value="<?= $veriler[$i]["ID"] ?>" onclick="aktifpasif(<?= $veriler[$i]['ID'] ?>,'<?= $kontrol[0]['tablo'] ?>')" ;>
+
+                            <label class="custom-control-label" for="customSwitch3<?= $veriler[$i]["ID"] ?>"></label>
+
+                          </div>
+
+                        </td>
+
+                        <td><?= $veriler[$i]["tarih"] ?></td>
+
+                        <td>
+
+                          <a href="<?= SITE ?>duzenle/<?= $kontrol[0]["tablo"] ?>/<?= $veriler[$i]["ID"] ?>" class="btn btn-warning btn-sm">Düzenle</a>
+
+                          <a href="<?= SITE ?>sil/<?= $kontrol[0]["tablo"] ?>/<?= $veriler[$i]["ID"] ?>" class="btn btn-danger btn-sm">Kaldır</a>
+
+                        </td>
+
+                      </tr>
+
+                  <?php
+
+                    }
+                  }
+
+
+
+                  ?>
+
+                  <!-----------------------------------------------------------/PHP--------------------------------------------------------------------------->
+
+
+
+                </tbody>
+
+                <tfoot>
 
                   <tr>
 
-                      <th>Sıra</th>
+                    <th>Sıra</th>
 
-                      <th>Açıklama</th>
+                    <th>Açıklama</th>
 
-                      <th>Durum</th>
+                    <th>Durum</th>
 
-                      <th>Tarih</th>
+                    <th>Tarih</th>
 
-                      <th>İşlem</th>
+                    <th>İşlem</th>
 
                   </tr>
 
-                  </tfoot>
+                </tfoot>
 
-                </table>
-
-              </div>
-
-              <!-- /.card-body -->
+              </table>
 
             </div>
 
-            <!-- /.card -->
+            <!-- /.card-body -->
+
+          </div>
+
+          <!-- /.card -->
 
 
 
 
 
-      </div><!-- /.container-fluid -->
+        </div><!-- /.container-fluid -->
 
-    </section>
+      </section>
 
-    <!-- /.content -->
+      <!-- /.content -->
 
-  </div>
+    </div>
 
-  
 
-  
+
+
 
   <?php
 
-  }
-
-    else{
-
-      ?>
-
-      <meta http-equiv="refresh" content="0;url=<?=SITE?>">
-
-      <?php
-
-
-
-    }
-
-  }
-
-else{
-
-    ?>
-
-      <meta http-equiv="refresh" content="0;url=<?=SITE?>">
-
-      <?php
-
-  }
+  } else {
 
   ?>
+
+    <meta http-equiv="refresh" content="0;url=<?= SITE ?>">
+
+  <?php
+
+
+
+  }
+} else {
+
+  ?>
+
+  <meta http-equiv="refresh" content="0;url=<?= SITE ?>">
+
+<?php
+
+}
+
+?>
